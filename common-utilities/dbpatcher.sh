870e16cc3d0cfb0f403432119132b55a8dbca664
RaunaqSuri <raunaq.suri@hotmail.com>
2014-02-11 14:45:34 -0500

    Updated script

--- common-utilities/dbpatcher.sh
+++ common-utilities/dbpatcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#call this script name with 7 parameters
-#./dbpatcher.sh 1 2 3 4 5 6
+#call this script name with 6 parameters
+#./dbpatcher.sh 1 2 3 4 5 6 7
 #The params are in this order:
-#DBDUMP, PATCHDIR, DBHOST, PORT, NAME, USER
-MINPARAM=6
+#DBDUMP, PATCHDIR, DBHOST, PORT, NAME, USER, PASS
+MINPARAM=7
 
 #checks to make sure that all parameters have arguments
 if [[ "$#" -ne $MINPARAM ]]; then
@@ -12,14 +12,14 @@ if [[ "$#" -ne $MINPARAM ]]; then
 fi
 
 
-DBDUMP=$1
-PATCHDIR=$2
-DBHOST=$3
-PORT=$4
-NAME=$5
-USER=$6 #make sure the user is a superuser
-
-
+DBDUMP=$1 #the database that you wish to restore
+PATCHDIR=$2 #directory where patches reside
+DBHOST=$3 #database host
+PORT=$4 #port number
+NAME=$5 #name of the new database
+USER=$6 #make sure the user has sufficient rights and privelges, such as createDB.
+PASS=$7
+export PGPASSWORD=${PASS} #allows user authentication without pgpass
 #Tests and connects to the DB host and quits immediately after
 psql -U ${USER} -h $DBHOST -p $PORT -d postgres -c "\q"
 FAIL=$?
@@ -37,6 +37,8 @@ DBCREATION="CREATE DATABASE ${NAME} WITH OWNER = ${USER};"
 #turns the name into lowercase
 NAME=$(echo $NAME| sed 's/.*/\L&/')
 
+
+#creates the new database
 psql -U $USER -h $DBHOST -p $PORT -d postgres -c "${DBCREATION}"
 FAIL=$?
 if [[ $FAIL -ne "0" ]]; then
@@ -45,6 +47,7 @@ if [[ $FAIL -ne "0" ]]; then
 fi
 
 #restores the database
+#the command also removes the users that are no longer present for the database
 gunzip -c $DBDUMP |sed "s#^\(ALTER TABLE .*\)\( OWNER TO \)\(.*\)\(;$\)#\1\2${USER}\4#g" | grep -v "GRANT SELECT ON TABLE"|sed "s#^\(REVOKE ALL ON TABLE .*\)seqware\(;$\)#\1${USER}\2#g"|sed "s#^\(GRANT ALL ON TABLE .*\)seqware\(;$\)#\1${USER}\2#g"| psql -U ${USER} -h $DBHOST -p $PORT -d ${NAME}
 FAIL=$?
  if [[ $FAIL -ne "0" ]]; then
@@ -54,8 +57,9 @@ FAIL=$?
  	exit $FAIL
  fi
 
- #applies the patches. The patch directory must make sure that the only files in it are patches
- for f in ${PATCHDIR}/*; do
+ #applies the patches. The patch directory must make sure that the only files in it are patches.
+ #NOTE: An error will appear when running the patch 1.0.2_to_1.0.3.sql. You can just ignore it
+ for f in ${PATCHDIR}/*.sql; do
  	echo $f
  	psql -U ${USER} -h ${DBHOST} -p ${PORT} ${NAME} < "$f"
  done
