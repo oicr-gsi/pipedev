39c176e5f97c51d6c72daf64f8e01626dc56c284
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-02-18 12:56:10 -0500

    Parameterize test database settings as a maven property (which is then made accessible to tests through systemPropertyVariables)

--- common-utilities/src/test/java/ca/on/oicr/pde/common/utilities/WebserviceManagerIT.java
+++ common-utilities/src/test/java/ca/on/oicr/pde/common/utilities/WebserviceManagerIT.java
@@ -7,42 +7,38 @@ import org.apache.commons.exec.CommandLine;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/** The test cases for webservice manager
- * The following test cases are included: creating a service then destroying it
- * creating multiple webservices
- * destroying multiple webservices
- * attempting to destroy a webservice that doesn't exist
- * ensuring that proper procedure is carried out incase the engine decides to cancel the job
- * @author Raunaq Suri
- *.
+/**
+ * The test cases for webservice manager The following test cases are included: creating a service then destroying it creating multiple webservices destroying
+ * multiple webservices attempting to destroy a webservice that doesn't exist ensuring that proper procedure is carried out incase the engine decides to cancel
+ * the job
+ *
+ * @author Raunaq Suri .
  */
-public class WebserviceManagerIT 
-{
+public class WebserviceManagerIT {
+
     /**
-     * The variables used. 
-     * Make sure that they change if you are unable to access some of them
+     * The variables used. Make sure that they change if you are unable to access some of them
      */
-    String dbdump = "src/main/resources/regression_test_database.sql.gz";
-    String patchDir = "src/main/resources/scripts";
-    String host = "seqbiopgsql.res.oicr.on.ca" ;
-    String port = "5432" ;
-    String user = "sqwci";
-    String pass = "Nie4Neon";
-    String seqDir = "~/seqware/seqware-webservice/";
+    String dbdump = System.getProperty("dbFile");
+    String patchDir = System.getProperty("dbPatchDir");
+    String host = System.getProperty("dbHost");
+    String port = System.getProperty("dbPort");
+    String user = System.getProperty("dbUser");
+    String pass = System.getProperty("dbPass");
+    String seqDir = System.getProperty("seqwareWebserviceDir");
     WebserviceManager service1 = new WebserviceManager(dbdump, patchDir, host, port, user, pass, seqDir);
     WebserviceManager service2 = new WebserviceManager(dbdump, patchDir, host, port, user, pass, seqDir);
     WebserviceManager service3 = new WebserviceManager(dbdump, patchDir, host, port, user, pass, seqDir);
     WebserviceManager service4 = new WebserviceManager(dbdump, patchDir, host, port, user, pass, seqDir);
     WebserviceManager service5 = new WebserviceManager(dbdump, patchDir, host, port, user, pass, seqDir);
-    
 
     /**
      * Creates two webservices with random db names
-     * @throws IOException 
+     *
+     * @throws IOException
      */
-   @Test
-    public void testCreationMinParams() throws IOException
-    {
+    @Test
+    public void testCreationMinParams() throws IOException {
         System.out.println("Attempting to create multiple webservices...");
         service1.setJavaHome("/oicr/local/lib/jvm/jdk1.6.0_10");
         service2.setJavaHome("/oicr/local/lib/jvm/jdk1.6.0_10");
@@ -51,69 +47,69 @@ public class WebserviceManagerIT
         System.out.println("Creating the second webservice");
         String url2 = service2.getUrl();
         System.out.println("This is the url of the first webservice: " + url);
-        System.out.println("This is the url of the second webservice: " +url2);
-        Assert.assertTrue( url.contains("/seqware-webservice"), "Error starting up webservice. This is the url given:" + url);
+        System.out.println("This is the url of the second webservice: " + url2);
+        Assert.assertTrue(url.contains("/seqware-webservice"), "Error starting up webservice. This is the url given:" + url);
     }
-    
+
     /**
      * Creates a webservice, then attempts to destroy it
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     @Test
-    public void testCreateDestroyService() throws IOException{
+    public void testCreateDestroyService() throws IOException {
         System.out.println("Attempting to create, then destroy, a webservice...");
         service3.setJavaHome("/oicr/local/lib/jvm/jdk1.6.0_10");
         String url3 = service3.getUrl();
-        System.out.println("Created a webservice with url "+url3);
+        System.out.println("Created a webservice with url " + url3);
 
         service3.destroy();
         System.out.println("Destroyed the webservice");
 
     }
-    
+
     /**
      * Destroys multiple webservices
-     * @throws IOException 
+     *
+     * @throws IOException
      */
-    
     @Test
-    public void testDestroyServices() throws IOException
-    {
+    public void testDestroyServices() throws IOException {
         System.out.println("Attempting to destroy multiple webservices... ");
         service1.destroy();
         System.out.println("First service destroyed");
         service2.destroy();
         System.out.println("Second service destroyed");
     }
-    
+
     /**
      * Attempts to destroy a service which doesn't exist
-     * @throws IOException 
-     */    
+     *
+     * @throws IOException
+     */
     @Test(expectedExceptions = IOException.class)
-    public void testNoServiceYetDestroy() throws IOException
-    {
+    public void testNoServiceYetDestroy() throws IOException {
         service4.setJavaHome("/oicr/local/lib/jvm/jdk1.6.0_10");
         System.out.println("Attempting to destroy a webservice which doesn't exist...");
         service4.destroy();
         Assert.fail("Exception was not thrown");
 
     }
-  
+
     /**
      * Simulates what occurs when the engine decides to cancel the job
-     * @throws IOException 
+     *
+     * @throws IOException
      */
-   
     @Test
-    public void testKilledJob() throws IOException{
+    public void testKilledJob() throws IOException {
         System.out.println("Attempting to see what would happen if the Engine kills the webservice...");
         service5.setJavaHome("/oicr/local/lib/jvm/jdk1.6.0_10");
         String url5 = service5.getUrl();
-        System.out.println("Webservice was killed by the engine" );
+        System.out.println("Webservice was killed by the engine");
 
         //Deletes the job beforehand so only the database is left
-         //sets up command to destroy webservice
+        //sets up command to destroy webservice
         StringBuilder destroyWeb = new StringBuilder();
         destroyWeb.append("qdel");
         destroyWeb.append(" ").append(service5.jobID);
@@ -131,7 +127,18 @@ public class WebserviceManagerIT
         //Now the job has been deleted, so I can attempt to destroy it
         System.out.println("Attempting to delete the webservice when the engine had already deleted it");
         service5.destroy(); //There is no webservice to be deleted, but the stray db shuold be deleted as well
-        
+
+    }
+
+    public static String getRequiredSystemProperty(String key) throws IllegalArgumentException {
+
+        String value = System.getProperty(key);
+
+        if (value == null) {
+            throw new IllegalArgumentException("Required system property [" + key + "] is not set.");
+        }
+
+        return value;
+
     }
 }
-    
