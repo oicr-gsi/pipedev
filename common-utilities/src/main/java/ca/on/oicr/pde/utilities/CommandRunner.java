3a184e979394e5a8925421ab96965c2bbd63c97a
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-03-25 15:15:44 -0400

    Correction to helpers and command runner package names

--- common-utilities/src/main/java/ca/on/oicr/pde/utilities/CommandRunner.java
+++ common-utilities/src/main/java/ca/on/oicr/pde/utilities/CommandRunner.java
@@ -1,4 +1,4 @@
-package ca.on.oicr.pde.common.utilities;
+package ca.on.oicr.pde.utilities;
 
 import java.io.File;
 import java.io.IOException;
---
+++ common-utilities/src/main/java/ca/on/oicr/pde/utilities/Helpers.java
@@ -0,0 +1,126 @@
+package ca.on.oicr.pde.utilities;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.testng.Assert;
+
+public class Helpers {
+
+    public static File getScriptFromResource(String scriptName) throws IOException {
+
+        File script = File.createTempFile(scriptName, ".sh");
+        script.setExecutable(true);
+        script.deleteOnExit();
+
+        InputStream resourceStream = Helpers.class.getClassLoader().getResourceAsStream(scriptName);
+        Assert.assertNotNull(resourceStream, String.format("Script resource [%s] was not found - verify that script exists as a resource.", scriptName));
+
+        FileUtils.writeStringToFile(script, IOUtils.toString(resourceStream));
+
+        return script;
+
+    }
+
+    public static String executeCommand(String id, String command, File workingDirectory, Map<String, String>... environmentVariables) throws IOException {
+
+        CommandLine c = new CommandLine("/bin/bash");
+        c.addArgument("-c");
+        c.addArgument(command, false);
+
+        CommandRunner cr = new CommandRunner();
+        cr.setCommand(c);
+        for (Map<String, String> e : environmentVariables) {
+            cr.setEnvironmentVariable(e);
+        }
+        cr.setWorkingDirectory(workingDirectory);
+
+        System.out.println(id + "{\n" + "Executing (with initial directory: " + workingDirectory + "):\n" + command.toString() + "\n}");
+
+        CommandRunner.CommandResult r = cr.runCommand();
+        Assert.assertTrue(r.getExitCode() == 0,
+                String.format("The following command returned a non-zero exit code [%s]:\n%s\nOutput from command:\n%s\n",
+                        r.getExitCode(), command, r.getOutput()));
+
+        return r.getOutput().trim();
+
+    }
+
+    public static File generateSeqwareSettings(File workingDirectory, String webserviceUrl, String schedulingSystem, String schedulingHost) throws IOException {
+
+        //TODO: implement this in java
+        StringBuilder command = new StringBuilder();
+        command.append(getScriptFromResource("generateSeqwareSettings.sh"));
+        command.append(" ").append(workingDirectory);
+        command.append(" ").append(webserviceUrl);
+        command.append(" ").append(schedulingSystem);
+        command.append(" ").append(schedulingHost);
+        command.append(" ").append(UUID.randomUUID());
+
+        return new File(executeCommand("", command.toString(), workingDirectory));
+
+    }
+
+    public static String buildPathFromDirectory(String initialPath, File dir) throws IOException {
+
+        File[] softwarePackages = dir.listFiles((FileFilter) DirectoryFileFilter.DIRECTORY);
+        Arrays.sort(softwarePackages);
+
+        StringBuilder path = new StringBuilder();
+        path.append(initialPath).append(":").append(dir);
+
+        for (File d : softwarePackages) {
+            path.append(":").append(d.getAbsolutePath());
+        }
+
+        return path.toString();
+
+    }
+
+    public static File getRequiredSystemPropertyAsFile(String propertyName) {
+
+        String value = System.getProperty(propertyName);
+
+        if (value == null || value.isEmpty()) {
+            throw new RuntimeException("The required property \"" + propertyName + "\" was not found.");
+        }
+
+        return new File(value);
+
+    }
+
+    public static String getRequiredSystemPropertyAsString(String propertyName) {
+
+        String value = System.getProperty(propertyName);
+
+        if (value == null || value.isEmpty()) {
+            throw new RuntimeException("The required property \"" + propertyName + "\" was not found.");
+        }
+
+        return value;
+
+    }
+
+    public static File generateTestWorkingDirectory(File workingDirectory, String prefix, String testName, String suffix) throws IOException {
+
+        //String prefix = new SimpleDateFormat("yyMMdd_HHmm").format(new Date()) + "_";
+        //String suffix = "_" + testId;
+        File testWorkingDirectory = new File(workingDirectory + "/" + prefix + "_" + testName + "_" + suffix + "/");
+
+        if (!testWorkingDirectory.mkdir()) {
+            throw new IOException("Cannot create directory because it already exists.");
+        }
+
+        return testWorkingDirectory;
+
+    }
+
+}
