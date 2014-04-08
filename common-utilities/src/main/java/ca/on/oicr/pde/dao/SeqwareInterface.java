b8d0e3d403df3e8f9ac9d3ada0018519ed559448
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-04-08 17:59:09 -0400

    Fix runtime sort error

--- common-utilities/src/main/java/ca/on/oicr/pde/dao/SeqwareInterface.java
+++ common-utilities/src/main/java/ca/on/oicr/pde/dao/SeqwareInterface.java
@@ -7,10 +7,12 @@ import ca.on.oicr.pde.model.ReducedFileProvenanceReportRecord;
 import ca.on.oicr.pde.model.Workflow;
 import ca.on.oicr.pde.model.WorkflowRun;
 import ca.on.oicr.pde.model.WorkflowRunReportRecord;
+import ca.on.oicr.pde.testing.decider.WorkflowRunReport;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Multimap;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -42,8 +44,14 @@ public abstract class SeqwareInterface {
             }
         }
 
-        Collections.sort(files);
-        
+        Collections.sort(files, new Comparator<ReducedFileProvenanceReportRecord>() {
+
+            @Override
+            public int compare(ReducedFileProvenanceReportRecord o1, ReducedFileProvenanceReportRecord o2) {
+                return o1.toString().compareTo(o2.toString());
+            }
+        });
+
         return files;
     }
 
