92623c3b32fb5e91c27a2a4e214527847cc3b5fe
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-08-19 11:29:01 -0400

    Update workflow run report test to provider current record count to record builder

--- common-utilities/src/test/java/ca/on/oicr/pde/testing/decider/WorkflowRunReportTest.java
+++ common-utilities/src/test/java/ca/on/oicr/pde/testing/decider/WorkflowRunReportTest.java
@@ -11,8 +11,8 @@ public class WorkflowRunReportTest {
     @Test
     public void testWorkflowRunReportCompare() {
 
-        FileProvenanceReportRecord f1 = new FileProvenanceReportRecord.Builder().setExperimentName("Test1").setFilePath("/tmp/1").setFileSwid("1").build();
-        FileProvenanceReportRecord f2 = new FileProvenanceReportRecord.Builder().setExperimentName("Test2").setFilePath("/tmp/2").setFileSwid("2").build();
+        FileProvenanceReportRecord f1 = new FileProvenanceReportRecord.Builder(1).setExperimentName("Test1").setFilePath("/tmp/1").setFileSwid("1").build();
+        FileProvenanceReportRecord f2 = new FileProvenanceReportRecord.Builder(2).setExperimentName("Test2").setFilePath("/tmp/2").setFileSwid("2").build();
 
         WorkflowRunReport expected = new WorkflowRunReport();
         expected.setFiles(Arrays.asList(new ReducedFileProvenanceReportRecord(f1), new ReducedFileProvenanceReportRecord(f2)));
