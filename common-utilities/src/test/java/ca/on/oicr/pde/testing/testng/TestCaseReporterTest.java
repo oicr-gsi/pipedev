e909ffad5a5c39c6c7eb167158f0769b693affd9
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-08-26 10:14:33 -0400

    Added a failure test to TestCaseReporterTest

--- common-utilities/src/test/java/ca/on/oicr/pde/testing/testng/TestCaseReporterTest.java
+++ common-utilities/src/test/java/ca/on/oicr/pde/testing/testng/TestCaseReporterTest.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
@@ -14,17 +15,23 @@ import org.testng.xml.XmlTest;
 
 public class TestCaseReporterTest {
 
+    TestNG t;
+    TestListenerAdapter tla;
+
+    @BeforeTest
+    public void setupTestNg(ITestContext context) {
+        t = new TestNG();
+        t.setOutputDirectory(context.getOutputDirectory());
+
+        tla = new TestListenerAdapter();
+        t.addListener(tla);
+    }
+
     @Test
-    public void test(ITestContext context) {
+    public void testCaseReporterTestOnTestSuccesses() {
 
         int numberOfTestCases = 10;
         int numberOfTestMethods = 2;
-
-        //Setup test runner
-        TestNG t = new TestNG();
-        t.setOutputDirectory(context.getOutputDirectory());
-        TestListenerAdapter tla = new TestListenerAdapter();
-        t.addListener(tla);
 
         //Setup test suite
         XmlSuite testSuite = new XmlSuite();
@@ -54,6 +61,28 @@ public class TestCaseReporterTest {
         Assert.assertEquals(tc.getPassedTests().size(), numberOfTestCases * numberOfTestMethods);
 
         Assert.assertEquals(t.getStatus(), 0);
+    }
+
+    @Test
+    public void testCaseReporterTestOnTestFailures() {
+
+        XmlSuite testSuite = new XmlSuite();
+        XmlTest testCase = new XmlTest(testSuite);
+        testCase.setXmlClasses(Arrays.asList(new XmlClass(ca.on.oicr.pde.testing.testng.FakeFailImpl.class)));
+
+        //Execute test suite
+        t.setXmlSuites(Arrays.asList(testSuite));
+        t.run();
+
+        ITestContext tc = tla.getTestContexts().get(0);
+        Assert.assertEquals(tla.getTestContexts().size(), 1, "The assumption that there is only one test listener no longer holds");
+
+        Assert.assertEquals(tc.getFailedTests().size(), 1);
+        Assert.assertEquals(tc.getSkippedTests().size(), 0);
+        Assert.assertEquals(tc.getPassedTests().size(), 0);
+
+        Assert.assertEquals(t.getStatus(), 0);
+
     }
 
 }
