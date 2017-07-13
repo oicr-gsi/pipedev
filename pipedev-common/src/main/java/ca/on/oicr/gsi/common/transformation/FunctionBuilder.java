c57be2bc50ca151702a0d53d1861f76de535ee0f
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2017-07-13 17:34:56 -0500

    Committing travisci and sonarcloud testing

--- pipedev-common/src/main/java/ca/on/oicr/gsi/common/transformation/FunctionBuilder.java
+++ pipedev-common/src/main/java/ca/on/oicr/gsi/common/transformation/FunctionBuilder.java
@@ -27,5 +27,10 @@ public class FunctionBuilder {
             }
         };
     }
-    
+// TODO remove me after sonarcloud does its thing
+public void junkMethodIsJunk(String unusedParam) throws Exception {
+    if (true) {
+        throw new Exception("this is garbage");
+    }
+}    
 }
